4c44c038e15ba72c7893c56b1fb90e536207c6f5
pedro-leoni <pedroleoni1996@gmail.com>
2022-05-11 17:45:26 -0300

    Indente app.js, termine el footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,14 +1,65 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styles from './Footer.module.css';
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import styles from './Footer.module.css';
+// import { Container } from "@mui/material";
+// import 
 
-export const Footer = () => {
-    return(
-        <div className={styles.footContainer}> 
-            <h6>Empresa copada</h6>
-            <p>Copyright © 2022 Empresa copada, Todos los derechos reservados.</p>
-            <p> Sobre Nosotros</p>
-        </div>
-    )
+// export const Footer = () => {
+//     return(
+//         <div className={styles.footContainer}> 
+//             <h6>Empresa copada</h6>
+//             <p>Copyright © 2022 Empresa copada, Todos los derechos reservados.</p>
+//             <p> Sobre Nosotros</p>
+//         </div>
+//     )
+// }
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="#E8E9F2">
+      {'Copyright © '}
+      <Link color="inherit" href="/about">
+        Empresa copada
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
+export const Footer = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        
+      }}
+    >
+    <CssBaseline />
+    <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: 'auto',
+          backgroundColor: '#494545',
+          color: '#E8E9F2'
+        }}
+    >
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            Todos los derechos reservados
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+    </Box>
+  );
+}
