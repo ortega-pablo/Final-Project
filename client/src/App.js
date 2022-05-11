da2ba33482e578062e3edf553e8a6275546db7f7
pedro-leoni <pedroleoni1996@gmail.com>
2022-05-11 13:10:52 -0300

    Creacion de paginado, modificacion en foother y home

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Detail } from "./components/Detail/Detail";
 import { Home } from "./components/Home";
+import { Footer } from "./components/Footer/Footer";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/detail" element={<Detail/>}/>
       </Routes>
+        <Footer/>
     </BrowserRouter>
   );
 }
