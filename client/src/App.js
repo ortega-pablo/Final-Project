35668a091bd3bf748b4a1aed780cf51fd777eaae
samueltribulo <samueltribulo@gmail.com>
2022-05-11 13:03:09 -0300

    Creación NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Detail } from "./components/Detail/Detail";
 import { Home } from "./components/Home";
-
+import {NavBar} from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar></NavBar>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/detail" element={<Detail/>}/>
       </Routes>
     </BrowserRouter>
+
   );
 }
 
