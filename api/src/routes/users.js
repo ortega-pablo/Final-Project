a19ee72764196bdba5d9812f7b81763d6ccbf229
JoseCcari <jose_13_99@hotmail.com>
2022-05-12 11:26:41 -0500

    register and login using bcrypt and jwt

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,23 +1,44 @@
 const { Router } = require("express");
-const { User } = require("../db");
 const router = Router();
+const jwt = require("jsonwebtoken");
+const { User } = require("../db");
+const bcrypt = require("bcrypt");
 
-router.post("/", async (req, res, next) => {
-  const { userName, email, password, firstName, lastName, phone, role } =
-    req.body;
+// Register User
+
+router.post("/create", async (req, res, next) => {
+  const { userName, email, password, firstName, lastName, phone } = req.body;
 
   try {
+    let Hashpassword = bcrypt.hashSync(password, 10);
     const newUser = await User.create({
       userName,
       email,
-      password,
+      password: Hashpassword,
       firstName,
       lastName,
       phone,
-      role,
     });
 
     res.status(200).send(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Login User
+
+router.post("/login", async (req, res, next) => {
+  const { userName, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { userName } });
+    console.log(user);
+    const passwordCorrect =
+      user === null ? false : await bcrypt.compare(password, user.password);
+    console.log(passwordCorrect);
+    console.log("aqui: ", await bcrypt.compare(password, user.password));
+    res.status(200).send("nice");
   } catch (error) {
     next(error);
   }
