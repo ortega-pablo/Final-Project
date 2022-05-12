7df80f0d1b980d300068b8020c98371a61a07308
Federico <fedeprenollio@gmail.com>
2022-05-11 23:12:26 -0300

    arreglo de cards en su componente y en el home

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,28 +1,66 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+// import React from 'react'
+// import { useNavigate } from 'react-router-dom'
 
-export const Card = ({name, price,brand, thumbnail}) => {
-  let navigate = useNavigate()
+// export const Card = ({name, price,brand, thumbnail}) => {
+//   let navigate = useNavigate()
 
-function handleDetail (e){
-  e.preventDefault()
-  navigate("/detail")
-}
+// function handleDetail (e){
+//   e.preventDefault()
+//   navigate("/detail")
+// }
 
 
-  return (
-    <>
-    <h2>{name}</h2>
-    <h3>$ {price}</h3>
-    <h3>{brand}</h3>
+//   return (
+//     <>
+//     <h2>{name}</h2>
+//     <h3>$ {price}</h3>
+//     <h3>{brand}</h3>
     
-    <img
-      src={thumbnail}
-      alt="soy la foto"/>
+//     <img
+//       src={thumbnail}
+//       alt="soy la foto"/>
 
-      <button onClick={(e)=>handleDetail(e)}>Detalle</button>
-      <button>Agregar al carrito</button>
-      <button>Comprar ahora</button>
-    </>
-  )
+//       <button onClick={(e)=>handleDetail(e)}>Detalle</button>
+//       <button>Agregar al carrito</button>
+//       <button>Comprar ahora</button>
+//     </>
+//   )
+// }
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+export default function MultiActionAreaCard({name, price,brand, thumbnail}) {
+  return (
+
+
+    <Card sx={{ width: 400 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={thumbnail}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <h3>$ {price}</h3>
+          <h3>{brand}</h3>
+          <Typography variant="body2" color="text.secondary">
+            Con esta minadora te vas a hacer millonario!
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Comparar
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,48 +1,74 @@
-import { SwipeableDrawer } from "@mui/material";
+import { Grid, SwipeableDrawer } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../redux/actions";
 import { Card } from "./Card/Card";
-import SwipeableTextMobileStepper from "./Carousel/SwipeableTextMobileStepper"
+import SwipeableTextMobileStepper from "./Carousel/SwipeableTextMobileStepper";
 import { Paginationxd } from "./Pagination/Pagination";
+import MultiActionAreaCard from "./Card/Card";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 export const Home = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products);
- 
+
   useEffect(() => {
     dispatch(getProducts());
   }, [dispatch]);
 
   //Pagination
-  const [currentPage, setCurrentPage] = useState(1)
-  const [productsPerPage, setProductsPerPage] = useState(9)
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage, setProductsPerPage] = useState(9);
   //calculos pagination
-  const lastProduct = currentPage*productsPerPage
-  const firstProduct = lastProduct - productsPerPage
-  const actualPage = products.slice(firstProduct, lastProduct)
+  const lastProduct = currentPage * productsPerPage;
+  const firstProduct = lastProduct - productsPerPage;
+  const actualPage = products.slice(firstProduct, lastProduct);
 
+  const classes = useStyles();
 
   return (
     <div>
       <h1>Soy el Home</h1>
-      <SwipeableTextMobileStepper/>
-
+      <SwipeableTextMobileStepper />
+      <Grid container spacing={12}>
       {products &&
         actualPage.map((prod, index) => {
           return (
-            <div key={index}>
-              <Card
-                key={index}
-                name={prod.name}
-                brand={prod.brand}
-                thumbnail={prod.thumbnail}
-                price={prod.price}
-              />
-            </div>
+            
+              <Grid item l>
+                <Paper className={classes.paper}>
+                  <MultiActionAreaCard
+                    key={index}
+                    name={prod.name}
+                    brand={prod.brand}
+                    thumbnail={prod.thumbnail}
+                    price={prod.price}
+                  />
+                </Paper>
+              </Grid>
+           
           );
         })}
-      <Paginationxd setCurrentPage={setCurrentPage} currentPage={currentPage} productsPerPage={productsPerPage} products={products.length}/>
+         </Grid>
+      <Paginationxd
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        productsPerPage={productsPerPage}
+        products={products.length}
+      />
     </div>
   );
 };
