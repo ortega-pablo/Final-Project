abc3f441316b47837c7ed4f4bde16d6ecc781aea
Juarez-Diego <diegoalbertojuarez@yahoo.com>
2022-05-13 18:53:55 -0500

    fixed GET Discounts, only missing Specification GET

--- api/src/routes/discounts.js
+++ api/src/routes/discounts.js
@@ -29,14 +29,34 @@ router.get("/", async (req, res, next) => {
   try{
     if(name) {
 
-      const findByName = await Discount.findAll()
+      const findByName = await Discount.findAll({
+        include: [
+          {
+            model: Product,
+            attributes: ["id", "name", "price"],
+            through: {
+              attributes: []
+            }
+          }
+        ]
+      })
       const found = await findByName?.filter(e => e.name.toLowerCase().includes(name.toLowerCase()));
 
       
       found.length ? res.status(200).json(found) : res.json("Discount not found, please try another search");
 
     } else {
-      const getAll = await Discount.findAll()
+      const getAll = await Discount.findAll({
+        include: [
+          {
+            model: Product,
+            attributes: ["id", "name", "price"],
+            through: {
+              attributes: []
+            }
+          }
+        ]
+      })
       return res.status(200).send(getAll)
     }
   } catch(error){
@@ -46,27 +66,5 @@ router.get("/", async (req, res, next) => {
 })
 
 
-
-router.get("/:discountId", async (req, res) => {
-
-  const {discountId} = req.params
-
-  try {
-    if(discountId) {
-      const findById = await Discount.findOne({
-        where: {
-          id: discountId
-        }
-      })
-  
-      return res.send(findById)
-  
-    } else{
-      return res.status(404).send("Discount not found")
-    }
-  } catch(error){
-    res.send(error)
-  }
-})
 
 module.exports = router;
