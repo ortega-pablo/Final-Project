2d1987d3d837f46693478fb2822f285778cfdb64
Juarez-Diego <diegoalbertojuarez@yahoo.com>
2022-05-13 18:42:18 -0500

    added GET Inventory, productId, get All

--- api/src/routes/productInventory.js
+++ api/src/routes/productInventory.js
@@ -33,18 +33,53 @@ router.get("/", async (req, res, next) => {
   const {productId} = req.query
 
   try{
-    if(productId) {
+   if(productId){
+    const getStockOne = await ProductInventory.findOne({
+      where: {
+        productId
+      },
+      include: [
+        {
+          model: Product,
+          attributes: ["name"]
+        }
+      ]
+    })
 
-      const findByQuantity = await Product.findAll()
-      const found = await findByQuantity?.filter(e => e.firstName.toLowerCase().includes(firstName.toLowerCase()));
-
-      
-      found.length ? res.status(200).json(found) : res.json("User not found, please try another search");
-
-    } else {
-      const getAll = await ProductInventory.findAll()
-      return res.status(200).send(getAll)
+    const info = {
+      name: getStockOne.product.name,
+      productId: getStockOne.productId,
+      quantity: getStockOne.quantity,
+      createdAt: getStockOne.createdAt,
+      updatedAt: getStockOne.updatedAt
     }
+
+   
+    return res.status(200).send(info)
+
+   } else {
+    const getAllInventory = await ProductInventory.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: ["name"]
+        }
+      ]
+    });
+
+    const mapped = getAllInventory.map(e => {
+      return {
+        name: e.product.name,
+        productId: e.productId,
+        quantity: e.quantity,
+        createdAt: e.createdAt,
+        updatedAt: e.updatedAt
+      }
+    })
+
+    return res.status(200).send(mapped)
+   }
+
   } catch(error){
     res.send(error)
   }
