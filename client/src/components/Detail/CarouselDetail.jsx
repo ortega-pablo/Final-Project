2c44ed979f03de7758052406d203a8c2b906218a
Pablo Ortega <p.ortegariera@gmail.com>
2022-05-15 16:05:39 -0300

    preparando para pull

--- client/src/components/Detail/CarouselDetail.jsx
+++ client/src/components/Detail/CarouselDetail.jsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import MobileStepper from "@mui/material/MobileStepper";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
