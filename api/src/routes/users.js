d0dcf05aa3bc276212bb2d59306cb2016c3560a3
JoseCcari <jose_13_99@hotmail.com>
2022-05-17 11:16:57 -0500

    user validation finish

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -12,9 +12,9 @@ router.post("/create", async (req, res, next) => {
 
   try {
     let Hashpassword = bcrypt.hashSync(password, 10);
-    const userFound = User.findOne({ where: { email } });
+    const userFound = await User.findOne({ where: { email } });
     if (userFound) {
-      return response.status(401).json({
+      return res.status(200).json({
         error: "email is already used",
       });
     }
@@ -27,7 +27,7 @@ router.post("/create", async (req, res, next) => {
       phone,
     });
 
-    res.status(200).send(newUser);
+    res.status(200).send("done");
   } catch (error) {
     next(error);
   }
@@ -44,7 +44,7 @@ router.post("/login", async (req, res, next) => {
     const passwordCorrect =
       user === null ? false : await bcrypt.compare(password, user.password);
     if (!(user && passwordCorrect)) {
-      response.status(401).json({
+      response.status(400).json({
         error: "invalid user or password",
       });
     }
