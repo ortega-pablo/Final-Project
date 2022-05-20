e5af9f58e8464ff7f52f2330a25689532ad6b3ee
Pablo Ortega <p.ortegariera@gmail.com>
2022-05-20 17:54:28 -0300

    ruta banners

--- api/src/routes/images.js
+++ api/src/routes/images.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const router = Router();
+const { BannerImages } = require("../db");
 const fs = require("fs");
 const upload = require("../middleware/multer")
 const cloudinary = require("../middleware/cloudinary")
 const axios = require ("axios")
 
-router.post("/upload", upload.array("image"), async (req, res, next)=>{
+router.post("/uploadProduct", upload.array("image"), async (req, res, next)=>{
   const {productId} = req.query
 
   const uploader = async (path) => await cloudinary.uploads(path,"Images")
@@ -18,11 +19,9 @@ router.post("/upload", upload.array("image"), async (req, res, next)=>{
       urls.push(newPath)
       fs.unlinkSync(path)
     }
-    console.log(urls)
 
     urls.map( async (el) =>  {
 
-      console.log(el.url)
       return await axios.post(`http://localhost:3001/products/addImage?urlFile=${el.url}&productId=${productId}`)
     })
 
@@ -34,6 +33,45 @@ router.post("/upload", upload.array("image"), async (req, res, next)=>{
     res.status(405).send("Images not uploaded successfully")
   }
 })
+
+
+router.post("/uploadBanner", upload.array("image"), async (req, res, next)=>{
+
+  const uploader = async (path) => await cloudinary.uploads(path,"Images")
+  if(req.method === "POST"){
+    const urls = []
+    const files = req.files
+    for(const file of files){
+      const { path } = file
+      const newPath = await uploader(path)
+      urls.push(newPath)
+      fs.unlinkSync(path)
+    }
+
+    urls.map( async (el) =>  {
+      await BannerImages.create({urlImage: el.url});
+    })
+    
+    res.status(200).send({
+      message: "Images uploaded successfully",
+      data: urls
+    })
+  }else {
+    res.status(405).send("Images not uploaded successfully")
+  }
+})
+
+router.get("/uploadBanner", async (req, res, next) => {
+  try {
+    const images = await BannerImages.findAll({
+      
+    });
+    res.status(200).send(images)
+  } catch (error) {
+    sen.status(400)
+  }
+});
+
 
 
 module.exports = router;
