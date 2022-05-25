1422095338d0c703e27f67b8ba8d35967257f156
Juarez-Diego <diegoalbertojuarez@yahoo.com>
2022-05-25 16:51:33 -0500

    edited User route, added product model to Asks

--- api/src/routes/orders.js
+++ api/src/routes/orders.js
@@ -1,6 +1,10 @@
 const { Router } = require("express");
-const { Product, User, Review, Order } = require("../db");
+const { Product, User, Review, Order, ShoppingCart } = require("../db");
 const router = Router();
+const Stripe = require("stripe");
+const {KEY_STRIPE} = process.env
+const stripe = new Stripe(KEY_STRIPE);
+
 
 router.post("/review", async (req, res, next) => {
   const { rating, review, orderId  } = req.body;
@@ -35,6 +39,50 @@ router.post("/review", async (req, res, next) => {
     next(error);
   }
 });
+
+
+
+
+
+router.post('/', async (req, res) => {
+
+  const {userId, productId } = req.query;
+  const {id, amount, total, state, address} = req.body;
+    try {
+        
+        const payment = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: "USD", 
+            description:"",
+            payment_method: id,
+            confirm: true,
+        })
+
+        // Aqui se agrega todo de la orden is paymen fue exitoso
+        if(payment){
+          const findUser = await User.findOne({
+            where: {
+              id: userId
+            }
+          })
+
+          
+
+
+        } else {
+
+        }
+
+        
+        res.send({message: "success"});
+        
+    } catch (error) {
+        console.log(error)
+        res.send({message: error.raw.message});
+    }
+})
+
+
 
 // router.post("/", async (req, res, next) => {
 
